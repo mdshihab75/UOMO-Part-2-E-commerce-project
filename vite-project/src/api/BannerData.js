d93a53b933b83f442729bb2f01fe345c11ae8124
mdshihab75 <mostakimshihab75@gmail.com>
2026-06-27 23:55:33 +0600

    banner part fixed

---
+++ vite-project/src/api/BannerData.js
@@ -0,0 +1,22 @@
+import bannerImage from '../assets/images/banner.png'
+export const BannerData = [
+    {
+        id: 1,
+        banner: bannerImage,
+    },
+
+    {
+        id: 2,
+        banner: bannerImage,
+    },
+
+    {
+        id: 3,
+        banner: bannerImage,
+    },
+
+    {
+        id: 4,
+        banner: bannerImage,
+    },
+]
